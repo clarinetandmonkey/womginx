8f4f7ebb3a421c3574c3b93cfa93f2edc532e5bb
clarinetandmonkey <69983667+clarinetandmonkey@users.noreply.github.com>
2021-11-04 08:56:23 -0400

    Update registration.php

--- public/registration.php
+++ public/registration.php
@@ -17,7 +17,7 @@ if (isset($_REQUEST['username'])){
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
 	$trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into `users` (username, password, email, trn_date)
+        $query = "INSERT into `users2` (username, password, email, trn_date)
 VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
         $result = mysqli_query($con,$query);
         if($result){
