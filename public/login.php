cd4d02d41c52b15afe20d24c8fe3158eb38a2ae0
clarinetandmonkey <69983667+clarinetandmonkey@users.noreply.github.com>
2021-11-04 08:56:45 -0400

    Update login.php

--- public/login.php
+++ public/login.php
@@ -17,7 +17,7 @@ if (isset($_POST['username'])){
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
 	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username'
+        $query = "SELECT * FROM `users2` WHERE username='$username'
 and password='".md5($password)."'";
 	$result = mysqli_query($con,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
